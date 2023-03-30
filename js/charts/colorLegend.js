16462b83292fc705f33d9ab18665f379cdc82d42
kylapurc <kylapurcell18@gmail.com>
2023-03-30 16:21:17 -0600

    clean up + add sources to read me

--- js/charts/colorLegend.js
+++ js/charts/colorLegend.js
@@ -16,22 +16,18 @@ class ColorLegend {
         // Setup bounds for chart
         vis.width = vis.config.width - vis.config.margin.left - vis.config.margin.right;
         vis.height = vis.config.height - vis.config.margin.top - vis.config.margin.bottom;
-          // Define size of SVG drawing area
-          vis.svg = d3.select(vis.config.svgElement)
-          .attr('width', vis.config.width)
-          .attr('height', vis.config.height);
+        // Define size of SVG drawing area
+        vis.svg = d3.select(vis.config.svgElement)
+            .attr('width', vis.config.width)
+            .attr('height', vis.config.height);
 
-      vis.chart = vis.svg.append('g')
-          .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+        vis.chart = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
     }
 
     updateVis() {
         const vis = this;
-
-        const sales = vis.sales;
-
-        console.log(sales);
 
         this.renderVis();
     }
@@ -40,33 +36,40 @@ class ColorLegend {
         const vis = this;
 
         // https://jsfiddle.net/k_sav/oa703n4j/ resource for legend
-       vis.chart.selectAll('text')
-      .data(vis.sales)
-      .join("text")
-      .attr("fill", d => vis.colorMap.get(d[0]))
-      .attr("x", function(d, i){ 
-        if (vis.sales.length <= 12) { if (i >= vis.sales.length/2) {
-        return 5;
-      } else {
-        return 200}}
-    else if (i > 6) {
-        return 5;
-    } else {
-        return 200
-    }})
-      .attr("y", function(d, i){ if(vis.sales.length <= 12){ if (i >= vis.sales.length/2) { return -5 + (15*i)}
-      else {
-        return  -5 + 15*(i + vis.sales.length/2)
-      }
-    } else if (i > 6) {
-        return -5 + (15*i)
+        vis.chart.selectAll('text')
+            .data(vis.sales)
+            .join("text")
+            .attr("fill", d => vis.colorMap.get(d[0]))
+            .attr("x", function (d, i) {
+                if (vis.sales.length <= 12) {
+                    if (i >= vis.sales.length / 2) {
+                        return 5;
+                    } else {
+                        return 200
+                    }
+                }
+                else if (i > 6) {
+                    return 5;
+                } else {
+                    return 200
+                }
+            })
+            .attr("y", function (d, i) {
+                if (vis.sales.length <= 12) {
+                    if (i >= vis.sales.length / 2) { return -5 + (15 * i) }
+                    else {
+                        return -5 + 15 * (i + vis.sales.length / 2)
+                    }
+                } else if (i > 6) {
+                    return -5 + (15 * i)
 
-    }
-     else {
-        return -5 + 15*(i + 6)
-     }} )
-      .text(function (d) {
-           return d[0]
-      })
+                }
+                else {
+                    return -5 + 15 * (i + 6)
+                }
+            })
+            .text(function (d) {
+                return d[0]
+            })
     }
 }
